c978c613bc37475c345636a8a7b4fcb110107178
Pavel <apdilwibim@gmail.com>
2020-10-15 02:31:39 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = Object.fromEntries(bracketsConfig);
+  let result = [];
+  for (let item of str.split('')) {
+    if (result[0] === item) {
+      result.shift();
+    } else if (array[item]) {
+      result.unshift(array[item])
+    } else {
+      return false;
+    }
+  }
+  if (result.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
